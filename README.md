b4eb79ebd1967a90a3cd44ff8dc57f1c98a956b6
Muhammad El-Hindi <melhindi@trustdble.com>
2022-09-07 08:41:37 +0200

    Added support for installing gRPC via FetchContent

--- README.md
+++ README.md
@@ -8,8 +8,36 @@ This is a basic example of a CMake project using Protobuf together with gRPC in 
 
 For some background info, have a look at this blog post explaining [how to structure gRPC projects with CMake](https://www.f-ax.de/dev/2020/11/08/grpc-plugin-cmake-support.html).
 
+## Project Dependencies
+The following dependencies exists:
+- CMake
+- gRPC / protobuf
+
+## Building the Project
+There are two ways that can be used to build the code of this repository:
+
+### Install dependencies manually
+As this project depends on gRPC and protobuf you can install gRPC and protobuf manually in a separate step.
+Please refer to this corresponding documentation of gRPC/protobuf to do that.
+You can also find an example for installing the dependencies in this project's `.github/workflows` yaml files.
+Once you installed all dependencies run the following commands to build the project:
+```
+cmake -B build
+cmake --build build
+```
+
+### Install dependencies via CMake FetchContent
+If you do not want to install the gRPC dependency on your system, you can also pull it in via CMake FetchContent.
+This will also pull protobuf as it is a dependency for gRPC.
+With this method you can simply use the following commands to build this project and its dependencies:
+```
+cmake -B build -DGRPC_FETCHCONTENT=TRUE -DGRPC_VERSION_TAG=v1.48.1
+cmake --build build
+```
+
+
 ### gRPC Reflection
-Reflection can be enabled by linking agains `gRPC::grpc++_reflection`, enabling support for the `grpc_cli` tool.
+Reflection can be enabled by linking against `gRPC::grpc++_reflection`, enabling support for the `grpc_cli` tool.
 
 If this project is linked with a static version of the grpc library from vcpkg
 the `-Wl,--whole-archive` flag has to be used. (together with `--allow-multiple-definition`). When linking dynamically,
---
+++ cmake/grpc.cmake
@@ -0,0 +1,91 @@
+# Copyright 2018 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# cmake build file for C++ route_guide example.
+# Assumes protobuf and gRPC have been installed using cmake.
+# See cmake_externalproject/CMakeLists.txt for all-in-one cmake build
+# that automatically builds all the dependencies before building route_guide.
+
+# this file is customized version of https://github.com/grpc/grpc/blob/master/examples/cpp/cmake/common.cmake
+
+cmake_minimum_required(VERSION 3.5.1)
+
+set (CMAKE_CXX_STANDARD 14)
+
+if(MSVC)
+  add_definitions(-D_WIN32_WINNT=0x600)
+endif()
+
+find_package(Threads REQUIRED)
+
+if(GRPC_FETCHCONTENT)
+  # Another way is to use CMake's FetchContent module to clone gRPC at
+  # configure time. This makes gRPC's source code available to your project,
+  # similar to a git submodule.
+  message(STATUS "Using gRPC via FetchContent. This will build gRPC from source and might take a while!")
+  include(FetchContent)
+  FetchContent_Declare(
+    grpc
+    GIT_REPOSITORY https://github.com/grpc/grpc.git
+    # when using gRPC, you will actually set this to an existing tag, such as
+    # v1.25.0, v1.26.0 etc..
+    # For the purpose of testing, we override the tag used to the commit
+    # that's currently under test.
+    GIT_TAG        ${GRPC_TAG_VERSION}
+  )
+  set(FETCHCONTENT_QUIET OFF) # enables us to see the progress of FetchContent build
+  FetchContent_MakeAvailable(grpc)
+  include(FindProtobuf) # this is required to include the protobuf cmake module that provides the protobuf_generate command
+
+  # Since FetchContent uses add_subdirectory under the hood, we can use
+  # the grpc targets directly from this build.
+  set(_PROTOBUF_LIBPROTOBUF libprotobuf)
+  set(_REFLECTION grpc++_reflection)
+  set(_PROTOBUF_PROTOC $<TARGET_FILE:protoc>)
+  set(_GRPC_GRPCPP grpc++)
+  if(CMAKE_CROSSCOMPILING)
+    find_program(_GRPC_CPP_PLUGIN_EXECUTABLE grpc_cpp_plugin)
+  else()
+    set(_GRPC_CPP_PLUGIN_EXECUTABLE $<TARGET_FILE:grpc_cpp_plugin>)
+  endif()
+else()
+  # This branch assumes that gRPC and all its dependencies are already installed
+  # on this system, so they can be located by find_package().
+
+  # Find Protobuf installation
+  # Looks for protobuf-config.cmake file installed by Protobuf's cmake installation.
+  set(protobuf_MODULE_COMPATIBLE TRUE)
+  find_package(Protobuf CONFIG REQUIRED)
+  message(STATUS "Using protobuf ${Protobuf_VERSION}")
+
+  set(_PROTOBUF_LIBPROTOBUF protobuf::libprotobuf)
+  set(_REFLECTION gRPC::grpc++_reflection)
+  if(CMAKE_CROSSCOMPILING)
+    find_program(_PROTOBUF_PROTOC protoc)
+  else()
+    set(_PROTOBUF_PROTOC $<TARGET_FILE:protobuf::protoc>)
+  endif()
+
+  # Find gRPC installation
+  # Looks for gRPCConfig.cmake file installed by gRPC's cmake installation.
+  find_package(gRPC CONFIG REQUIRED)
+  message(STATUS "Using gRPC ${gRPC_VERSION}")
+
+  set(_GRPC_GRPCPP gRPC::grpc++)
+  if(CMAKE_CROSSCOMPILING)
+    find_program(_GRPC_CPP_PLUGIN_EXECUTABLE grpc_cpp_plugin)
+  else()
+    set(_GRPC_CPP_PLUGIN_EXECUTABLE $<TARGET_FILE:gRPC::grpc_cpp_plugin>)
+  endif()
+endif()
